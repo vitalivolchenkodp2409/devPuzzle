847da0270b3cc5cc887eeae35a0dbf4b604701ec
vitaliyvolchenko <vitaliyvolchenkodeveloper@gmail.com>
2018-12-05 10:57:43 +0200

    Fix Cors middleware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -6,16 +6,11 @@ use Closure;
 
 class Cors {
 
-    public function handle($request, Closure $next) {     
-        $allowedOrigins = ['http://myroute.xyz', 'http://clarkconcepts.net','http://localhost'];
-        $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
-        if (in_array($origin, $allowedOrigins)) {
-            return $next($request)
-                ->header('Access-Control-Allow-Origin', $origin)
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With, cache-control,postman-token, token')
-                ->header('Access-Control-Allow-Credentials',' true');
-        }
-        return $next($request);
-    }
+  public function handle($request, Closure $next)
+  {
+      return $next($request)
+      ->header('Access-Control-Allow-Origin','*')
+      ->header('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE, OPTIONS')
+      ->header('Access-Control-Allow-Headers','Content-Type, Authorization, X-Requested-With');
+  }
 }

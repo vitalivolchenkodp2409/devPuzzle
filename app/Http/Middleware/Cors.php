401db73405996db3241a561c0968c450b8dfa50d
vitaliyvolchenko <vitaliyvolchenkodeveloper@gmail.com>
2018-12-11 17:11:23 +0200

    Fix Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,6 +19,6 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin','*')
             ->header('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers','Content-Type, Authorization, X-Requested-With');
+            ->header('Access-Control-Allow-Headers','*');
     }
 }
